dd0fe1073cc11b9ea77bc9b1e31edfd831ea6c8d
Adriano Langaro <adriano@hgv.inf.br>
2017-12-17 21:57:14 -0200

    Pequenas alterações

--- WordFitness.py
+++ WordFitness.py
@@ -3,8 +3,3 @@ from funcoes.crack_ga import *
 goal = "banana"
 firstGeneration = generateFirstPopulation(500, goal)
 print(Evolution(firstGeneration, goal, 1));
-
-
-
-    #for x in range(0,500):
-    #    print("We're on time %d" % (x))
